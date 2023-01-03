1653e486b0fafc4c64a10d069c9de0a4e6b56e12
Bahri Hırfanoğlu <bahrihrf34@gmail.com>
2023-01-03 23:50:30 +0300

    auth service: log service connection created.

--- auth-service/src/actions/sendMessage.js
+++ auth-service/src/actions/sendMessage.js
@@ -1,6 +1,16 @@
+const LogService = require("../services/logService"),
+  hosts = require("../protos/main/host.json");
+
 exports.SendMessage = async function (context) {
   try {
     const { data } = context.req;
+    const logServiceHost = hosts["logService.proto"];
+    const logServiceInstance = new LogService(
+      `${logServiceHost.host}:${logServiceHost.port}`
+    );
+    if (logServiceInstance) {
+      logServiceInstance.InfoLog(`Auth Service SendMessage: ${data}`, 1);
+    }
     context.res = {
       status: 200,
       message: data,
